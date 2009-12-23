eafdf3098e2aa735b1fe27ccc0a96a1241b105b4
Rabin Vincent <rabin.vincent@stericsson.com>
2009-12-23 21:37:44 +0530

    mmc-u8500: restrict blk count for polling mode
    Signed-off-by: Rabin Vincent <rabin.vincent@stericsson.com>

--- drivers/mmc/host/mmc-u8500.c
+++ drivers/mmc/host/mmc-u8500.c
@@ -1507,7 +1507,10 @@ static int u8500_mmci_probe(struct amba_device *dev, void *id)
 	else
 #endif
 	mmc->max_blk_size = 4096;
-	mmc->max_blk_count = mmc->max_req_size;
+	if (devicemode == MCI_DMAMODE)
+		mmc->max_blk_count = mmc->max_req_size;
+	else
+		mmc->max_blk_count = 64;
 	spin_lock_init(&host->lock);
 	host->clk = clk_get(NULL, dev->dev.bus_id);
 	if (!host->clk) {
