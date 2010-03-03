4c3314bdd1622ea6392d85e1b232d11afe20aedc
Rabin Vincent <rabin.vincent@stericsson.com>
2010-03-03 18:59:19 +0530

    nomadik-gpio: remove gpio_to_irq
    Signed-off-by: Rabin Vincent <rabin.vincent@stericsson.com>

--- arch/arm/plat-nomadik/include/plat/gpio.h
+++ arch/arm/plat-nomadik/include/plat/gpio.h
@@ -22,7 +22,6 @@
 #define gpio_get_value  __gpio_get_value
 #define gpio_set_value  __gpio_set_value
 #define gpio_cansleep   __gpio_cansleep
-#define gpio_to_irq     __gpio_to_irq
 
 /*
  * "nmk_gpio" and "NMK_GPIO" stand for "Nomadik GPIO", leaving
