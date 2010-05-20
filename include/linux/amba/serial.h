a77602bac1f13cfbd9b907983b0ddbca6a1beaac
Jonas Aaberg <jonas.aberg@stericsson.com>
2010-05-20 16:20:39 +0200

    amba-pl011: Added ST specific changes

--- include/linux/amba/serial.h
+++ include/linux/amba/serial.h
@@ -36,11 +36,13 @@
 #define UART010_LCRL		0x10	/* Line control register, low byte. */
 #define UART010_CR		0x14	/* Control register. */
 #define UART01x_FR		0x18	/* Flag register (Read only). */
+#define ST_UART011_LCRH_RX	0x1C    /* Rx line control register. */
 #define UART010_IIR		0x1C	/* Interrupt indentification register (Read). */
 #define UART010_ICR		0x1C	/* Interrupt clear register (Write). */
 #define UART01x_ILPR		0x20	/* IrDA low power counter register. */
 #define UART011_IBRD		0x24	/* Integer baud rate divisor register. */
 #define UART011_FBRD		0x28	/* Fractional baud rate divisor register. */
+#define ST_UART011_LCRH_TX	0x2c    /* Tx Line control register. */
 #define UART011_LCRH		0x2c	/* Line control register. */
 #define UART011_CR		0x30	/* Control register. */
 #define UART011_IFLS		0x34	/* Interrupt fifo level select. */
